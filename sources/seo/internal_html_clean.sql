646a577fbe455d4820830ea97b8b3ea702851b1f
Tommi Saltiola <tommi@tommisaltiola.com>
2024-10-15 22:41:29 -0600

    remove sources

--- sources/seo/internal_html_clean.sql
+++
@@ -1,5 +0,0 @@
-SELECT *
-FROM main.internal_html_clean
-WHERE (Address LIKE '%${domain_name}/%' 
-   OR Address LIKE '%${domain_name}')
-  AND indexability_status NOT IN ('Canonicalised', 'Redirected', 'noindex'); 
--- sources/seo/pagespeed_mobile.sql
+++
@@ -1,3 +0,0 @@
-SELECT * FROM pagespeed_mobile
-WHERE (url LIKE '%${domain_name}/%' 
-   OR url LIKE '%${domain_name}');
